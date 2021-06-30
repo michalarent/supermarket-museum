bdd542396f4d2e73b4afee07d92681591553d186
michalarent <arent.michal@gmail.com>
2021-06-30 16:02:14 +0200

    removed from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,9 @@
-import React from "react";
 import "../styles/globals.css";
 import "../styles/maps.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/inksplash.css";
 
 function MyApp({ Component, pageProps }) {
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
   return <Component {...pageProps} />;
 }
 
