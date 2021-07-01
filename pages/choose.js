8501e8d5bdf1a1a9ea91a9842fe1d1d685f6c509
michalarent <arent.michal@gmail.com>
2021-07-01 13:11:11 +0200

    added prefetch to choose page

--- pages/choose.js
+++ pages/choose.js
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import SupermarketMap from "../components/maps/SupermarketMap";
 import styles from "../styles/Home.module.css";
 import styles_map from "../styles/map.module.css";
-import Router from "next/router";
+import { useRouter } from "next/router";
 import Fade from "@material-ui/core/Fade";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import Grid from "@material-ui/core/Grid";
@@ -21,14 +21,6 @@ import {
 } from "../api/graphcms";
 import { IconButton } from "@material-ui/core";
 
-export async function getStaticProps() {
-  const { infoPages } = await getAllAgroPermaLabInfo();
-
-  return {
-    props: { infoPages },
-  };
-}
-
 const useStyles = makeStyles((theme) => ({
   root: {},
   grid: {
@@ -37,18 +29,26 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Choose({ infoPages }) {
+export default function Choose() {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    router.prefetch("/garden");
+    router.prefetch("/museum");
+  }, []);
+
   async function handleTransitionMuseum() {
     setShowTransition(true);
     await timeout(500);
-    Router.push("/museum");
+    router.push("/museum");
   }
 
   const [show, setShow] = React.useState(true);
+
   async function handleTransitionGarden() {
     setShowTransition(true);
     await timeout(500);
-    Router.push("/garden");
+    router.push("/garden");
   }
 
   const [showTransition, setShowTransition] = React.useState(false);
@@ -87,19 +87,23 @@ export default function Choose({ infoPages }) {
         <Fade in={true} timeout={500}>
           <Grid container className={styles.landingPage}>
             <Grid item xs={12} md={6} lg={6}>
-              <div className={styles.supermarketMiniContainer}>
+              <div
+                className={styles.supermarketMiniContainer}
+                onClick={handleTransitionMuseum}
+              >
                 <img
                   className={styles.supermarketMini}
-                  onClick={() => handleTransitionMuseum}
                   src="/supermarket/supermarketGrubszy.png"
                 ></img>
               </div>
             </Grid>
             <Grid item xs={12} md={6} lg={6}>
-              <div className={styles.gardenMiniContainer}>
+              <div
+                className={styles.gardenMiniContainer}
+                onClick={handleTransitionGarden}
+              >
                 <img
                   className={styles.gardenMini}
-                  onClick={() => handleTransitionGarden}
                   src="/supermarket/gardenGrubszy.png"
                 ></img>
               </div>
